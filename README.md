b5068e5be21f3735ccc83df133e342d6273f0e89
quocanuit <quocanuit@gmail.com>
2025-09-20 08:48:33 +0700

    chore: add set up script

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # MLSecOps
 
+## Quick set up in Colab
+
+```
+!git clone https://github.com/quocanuit/mlsecops.git
+```
+```
+import os
+
+os.chdir("/content/mlsecops")
+
+print("Current working directory:", os.getcwd())
+```
+```
+os.environ["AWS_ACCESS_KEY_ID"] ="YOURS"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "yours"
+os.environ["AWS_REGION"] = "us-east-1"
+```
+```
+!sh set_up_dvc_notebook.sh
+```
+
 ## How to use DVC
 
 ```bash
-# Pull data
+# Pull data (pulled in set_up script)
 dvc pull
 
 # Add/Update data
---
+++ set_up.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ] || [ -z "$AWS_REGION" ]; then
+    echo "Error: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_REGION not set." >&2
+    exit 1
+fi
+
+pip install -r "requirements.txt"
+
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile mlsecops_user
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile mlsecops_user
+aws configure set region "$AWS_REGION" --profile mlsecops_user
+aws configure set output "text" --profile mlsecops_user
+echo "AWS CLI profile 'mlsecops_user' configured."
+
+export AWS_PROFILE=mlsecops_user
+
+dvc pull
+
+DATASET_PATH="/content/ICAIF_KAGGLE/"
+echo "Dataset path: $(realpath "$DATASET_PATH")"
