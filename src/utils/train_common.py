b84b1241755dab3bc5d39410aab00b3432727b1d
quocanuit <quocanuit@gmail.com>
2025-10-22 23:11:49 +0700

    chore: finalize

--- src/utils/train_common.py
+++ src/utils/train_common.py
@@ -21,7 +21,7 @@ ROOT = Path(os.getcwd())
 
 
 def load_preprocessed_data():
-    preprocessed_data_dir = ROOT / "artifacts" / "preprocessed"
+    preprocessed_data_dir = ROOT / "data" / "preprocessed"
 
     X_train_resampled = pd.read_csv(preprocessed_data_dir / "X_train_resampled.csv").values
     y_train_resampled = pd.read_csv(preprocessed_data_dir / "y_train_resampled.csv").values.ravel()
